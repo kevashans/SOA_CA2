e02101b735d1a74909341062267ce76626173f47
gursimar03 <gursimar03@gmail.com>
2024-12-07 14:02:58 +0000

    added dashboard routing

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,12 +1,59 @@
-import './App.css';
-import { Login } from './features/login/Login';
+import * as React from 'react';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { AppProvider } from '@toolpad/core/react-router-dom';
+import { useNavigate , Outlet } from "react-router-dom";
+import type { Navigation, Session } from '@toolpad/core';
+import { SessionContext } from './SessionContext';
 
-function App() {
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'orders',
+    title: 'Orders',
+    icon: <ShoppingCartIcon />,
+  },
+];
+
+const BRANDING = {
+  title: 'Chatbox',
+};
+
+export default function App() {
+  const [session, setSession] = React.useState<Session | null>(null);
+  const navigate = useNavigate();
+
+  const signIn = React.useCallback(() => {
+    navigate('/sign-in');
+  }, [navigate]);
+
+  const signOut = React.useCallback(() => {
+    setSession(null);
+    navigate('/sign-in');
+  }, [navigate]);
+
+  const sessionContextValue = React.useMemo(
+    () => ({ session, setSession }),
+    [session, setSession],
+  );
+
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <SessionContext.Provider value={sessionContextValue}>
+      <AppProvider
+        navigation={NAVIGATION}
+        branding={BRANDING}
+        session={session}
+        authentication={{ signIn, signOut }}
+      >
+        <Outlet />
+      </AppProvider>
+    </SessionContext.Provider>
   );
 }
-
-export default App;
